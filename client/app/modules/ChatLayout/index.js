e8a5cf9945b2c94067d8e37697cd8c05cafcbb74
Ahmed Saad <a7med.s3d.ibrahim@gmail.com>
2018-04-25 20:05:09 +0200

    something

--- client/app/modules/ChatLayout/index.js
+++ client/app/modules/ChatLayout/index.js
@@ -1,104 +1,47 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 // Importing Chat Components
-import { ThemeProvider, MessageList, Message, MessageText } from '@livechat/ui-kit';
+import { MessageBox, ChatItem, Input, Button } from "react-chat-elements";
 // Importing Action Creators
-import { ADD_USER } from './chatActions';
-
-const theme = {
-    vars: {
-        'primary-color': '#427fe1',
-        'secondary-color': '#fbfbfb',
-        'tertiary-color': '#fff',
-        'avatar-border-color': 'blue'
-    },
-    MessageList: {
-        Avatar: {
-            size: '42px'
-        },
-        css: {
-            backgroundColor: '#F1F1F1',
-            borderColor: 'black'
-        }
-    },
-    Message: {
-        css: {
-            fontWeight: 'bold',
-            fontSize: '1.5rem'
-        }
-    }
-};
+import { ADD_USER } from "./chatActions";
 
 class ChatLayout extends Component {
-    render() {
-        return (
-            <ThemeProvider theme={theme} >
-                <MessageList>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Snow">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message isOwn authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                    <Message authorName="Jon">
-                        <MessageText>Hello! I am Jon!</MessageText>
-                    </Message>
-                </MessageList>
-            </ThemeProvider>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <MessageBox>
+          <ChatItem
+            title={"Facebook"}
+            subtitle={"What are you doing?"}
+            date={new Date()}
+            unread={0}
+          />
+        </MessageBox>
+        <Input
+          placeholder="Type here..."
+          buttons={
+            <Button color="white" backgroundColor="black" text="Send" />
+          }
+        />
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        form: state.form
-    };
+  return {
+    form: state.form
+  };
 }
 
 function matchDispatchToProps(dispatch) {
-    return bindActionCreators({
-        handleSubmit: ADD_USER
-    }, dispatch);
+  return bindActionCreators(
+    {
+      handleSubmit: ADD_USER
+    },
+    dispatch
+  );
 }
 
 export default connect(mapStateToProps, matchDispatchToProps)(ChatLayout);
