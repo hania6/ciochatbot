9e9a5994fef6ad7e7ea3d81c98bd9a880a715cc8
Ahmed Saad <a7med.s3d.ibrahim@gmail.com>
2018-04-25 19:49:55 +0200

    Chat Layout 1

--- client/app/modules/App/index.js
+++ client/app/modules/App/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-
+import styled from 'styled-components';
 // Loading Modules
-import ContainerComp from '../ContainerComp';
+import ChatLayout from '../ChatLayout';
 
 export default class App extends React.Component {
     render() {
         return (
-            <div>
-                <div>
-                    {this.props.children}
-                    <ContainerComp />
-                </div>
-            </div>
+            <Container>
+                <div>Charts we keda</div>
+                <ChatContainer>
+                    <ChatLayout />
+                </ChatContainer>
+            </Container>
         );
     }
 }
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    overflow-x: hidden;
+    background-color: #F3E4A3;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+`;
+
+const ChatContainer = styled.div`
+    border-radius: 20px;
+    border: 4px solid black;
+    display: inline-block;
+    height: 100vh;
+    width: 100%;
+    overflow-y: scroll;
+`;
--- client/app/store.js
+++ client/app/store.js
@@ -2,12 +2,12 @@ import { applyMiddleware, createStore, combineReducers, compose } from "redux";
 import thunk from "redux-thunk";
 
 // Import Reducers
-import formReducer from "./modules/ContainerComp/formReducer";
+import chatReducer from "./modules/ChatLayout/chatReducer";
 
 const middleware = applyMiddleware(thunk);
 
 const store = createStore(combineReducers({
-    form: formReducer
+    chat: chatReducer
 }), compose(middleware, window.devToolsExtension ? window.devToolsExtension() : f => f));
 
 export default store;
