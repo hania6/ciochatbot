634d3d85f18e07c2810ccb9c9d4ed2519fe1ca49
Ahmed Saad <a7med.s3d.ibrahim@gmail.com>
2018-05-06 04:15:03 +0200

    added config file

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //Watson Assistant Credentials
+  WA_workspace_id: 'c3987e4d-644c-45d6-9e85-57178fb1c2e9',
+  WA_username: '216ced35-b8c3-46ab-abfc-b4f2452eb1f7',
+  WA_password: 'GlJRFcjTqSgZ',
+  //Database Credentials
+  DBCreds: 'Some Credentials'
+}
