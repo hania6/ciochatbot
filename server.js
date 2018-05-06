a0c4dd22a832cb0543d6ab7c69d7da6fcc73c554
Ahmed Saad <a7med.s3d.ibrahim@gmail.com>
2018-05-06 04:10:26 +0200

    fixed routes in server.js

--- server.js
+++ server.js
@@ -53,11 +53,11 @@ if (isDev) {
     }));
 
     app.use(webpackHotMiddleware(compiler));
-    app.use(express.static(path.resolve(__dirname, '../dist')));
+    app.use(express.static(path.resolve(__dirname, './dist')));
 } else {
-    app.use(express.static(path.resolve(__dirname, '../dist')));
+    app.use(express.static(path.resolve(__dirname, './dist')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+        res.sendFile(path.resolve(__dirname, './dist/index.html'));
         res.end();
     });
 }
